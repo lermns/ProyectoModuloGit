bc0b264ae687e8b32f0fdf7442d1ae150ee7260b
lermns <htcmarchi@gmail.com>
2026-02-18 12:35:34 +0100

    add new func SaludosRand

--- suma.go
+++ suma.go
@@ -1,5 +1,9 @@
 package proyectoModuloGit
 
+import (
+	"math/rand"
+)
+
 // Sumar suma dos números
 func Sumar(a, b int) int {
 	return a + b
@@ -8,4 +12,9 @@ func Sumar(a, b int) int {
 // Saludar retorna un saludo personalizado
 func Saludar(nombre string) string {
 	return "Hola, " + nombre + "!"
+}
+
+func SaludarRandom() string {
+	randomValues := []string{"Hola", "Hi", "Hey", "Saludos", "Greatings"}
+	return randomValues[rand.Intn(len(randomValues))]
 }
