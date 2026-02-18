56e121a809ac495846a07e10929db05b2154184a
lermns <htcmarchi@gmail.com>
2026-02-18 11:43:21 +0100

    Initial commit

---
+++ cmd/suma.go
@@ -0,0 +1,11 @@
+package proyectoModuloGit
+
+// Sumar suma dos números
+func Sumar(a, b int) int {
+	return a + b
+}
+
+// Saludar retorna un saludo personalizado
+func Saludar(nombre string) string {
+	return "Hola, " + nombre + "!"
+}
